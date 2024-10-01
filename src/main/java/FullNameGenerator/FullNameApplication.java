94b30d08afc5d69f37771ab777abba05a59030b5
sabuharb <abuharb.sandy@gmail.com>
2024-10-01 11:52:57 -0500

    1st commit

---
+++ src/main/java/FullNameGenerator/FullNameApplication.java
@@ -0,0 +1,29 @@
+package FullNameGenerator;
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your first name: ");
+        String firstName = scanner.nextLine().trim();
+
+        System.out.print("Please enter your middle name (press enter if none): ");
+        String middleName = scanner.nextLine().trim();
+
+        System.out.print("Please enter your last name: ");
+        String lastName = scanner.nextLine().trim();
+
+        System.out.print("Please enter your suffix (press enter if none): ");
+        String suffix = scanner.nextLine().trim();
+
+        if (!suffix.isEmpty()) {
+            String fullName = ", " + suffix;
+
+            System.out.println("Full name: " + fullName);
+        }
+
+
+
+    }
+}
